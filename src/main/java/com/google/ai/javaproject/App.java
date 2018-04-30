eae1d0dd091f91a2c69043625ab16daa9194adda
msb <msb@msb-PC>
2018-04-30 11:15:37 +0530

    My first Commit

---
+++ src/main/java/com/google/ai/javaproject/App.java
@@ -0,0 +1,13 @@
+package com.google.ai.javaproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
